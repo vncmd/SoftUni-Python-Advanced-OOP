6f2273e7afae68af4cc4078f5f75e4b5b64955bb
Vladislav Naydenov Dimov <99009455+vncmd@users.noreply.github.com>
2023-05-10 10:46:59 +0200

    Output program helps keep track of customers in a supermarket.

---
+++ [1.1] Lists as Stacks and Queues/[0] Lists as Stacks and Queues - Lab/03_supermarket.py
@@ -0,0 +1,18 @@
+from collections import deque
+
+customers = deque()
+
+while True:
+    data = input()
+    if data == "Paid":
+        while customers:
+            print(customers.popleft())
+
+        continue
+
+    elif data == "End":
+        break
+
+    customers.append(data)
+
+print(f"{len(customers)} people remaining")
