682d9e4ab6e430e2baa3b720848c863d0adaa561
Vladislav Naydenov Dimov <99009455+vncmd@users.noreply.github.com>
2023-05-13 08:56:14 +0200

    Python Advanced: Tuples and Sets - Exercises
    Problem: n° 5

---
+++ [1.2] Tuples and Sets/[1] Tuples and Sets - Exercises/05_longest_intersection.py
@@ -0,0 +1,19 @@
+longest_intersection = set()
+
+n_data = int(input())
+for _ in range(n_data):
+    first_data, second_data = [el.split(",") for el in input().split("-")]
+
+    first_range = set(range(int(first_data[0]), int(first_data[1]) + 1))
+    second_range = set(range(int(second_data[0]), int(second_data[1]) + 1))
+
+    intersection = first_range.intersection(second_range)
+
+    if len(intersection) > len(longest_intersection):
+        longest_intersection = intersection
+
+print(
+    f"Longest intersection is "
+    f"[{', '.join(str(x) for x in longest_intersection)}] "
+    f"with length {len(longest_intersection)}"
+)
