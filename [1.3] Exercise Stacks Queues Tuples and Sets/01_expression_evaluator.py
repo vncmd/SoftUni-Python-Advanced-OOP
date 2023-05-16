1a983aefe403506a4b2fdebacc3d001f305ba52e
Vladislav Naydenov Dimov <99009455+vncmd@users.noreply.github.com>
2023-05-16 06:07:59 +0200

    Python Advanced: Exercise: Stacks, Queues, Tuples and Sets
    Problem: n° 1

---
+++ [1.3] Exercise Stacks Queues Tuples and Sets/01_expression_evaluator.py
@@ -0,0 +1,47 @@
+first = set(int(x) for x in input().split())
+second = set(int(x) for x in input().split())
+
+functions = {
+    "Add First": lambda x: [first.add(el) for el in x],
+    "Add Second": lambda x: [second.add(el) for el in x],
+    "Remove First": lambda x: [first.discard(el) for el in x],
+    "Remove Second": lambda x: [second.discard(el) for el in x],
+    "Check Subset": lambda x: print(first.issubset(second) or second.issubset(first)),
+}
+
+for _ in range(int(input())):
+    first_command, second_command, *data = input().split()
+
+    command = first_command + " " + second_command
+
+    functions[command]([int(x) for x in data])
+
+
+print(*sorted(first), sep=", ")
+print(*sorted(second), sep=", ")
+
+
+
+# Different Solution:
+
+# first = set(int(x) for x in input().split())
+# second = set(int(x) for x in input().split())
+#
+# for _ in range(int(input())):
+#     first_command, second_command, *data = input().split()
+#
+#     command = first_command + " " + second_command
+#
+#     if command == "Add First":
+#         [first.add(int(el)) for el in data]
+#     elif command == "Add Second":
+#         [second.add(int(el)) for el in data]
+#     elif command == "Remove First":
+#         [first.discard(int(el)) for el in data]
+#     elif command == "Remove Second":
+#         [second.discard(int(el)) for el in data]
+#     else:
+#         print(first.issubset(second) or second.issubset(first))
+#
+# print(*sorted(first), sep=", ")
+# print(*sorted(second), sep=", ")
