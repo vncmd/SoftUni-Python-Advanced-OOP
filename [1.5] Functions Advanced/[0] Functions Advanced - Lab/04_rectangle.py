3d4886027648c7566bb32d56539db54d15f639b9
Vladislav Naydenov Dimov <99009455+vncmd@users.noreply.github.com>
2023-05-23 14:10:16 +0200

    Python Advanced: Functions Advanced - Lab Problem: n° 4

---
+++ [1.5] Functions Advanced/[0] Functions Advanced - Lab/04_rectangle.py
@@ -0,0 +1,12 @@
+def rectangle(length, width):
+    if not isinstance(length, int) or not isinstance(width, int):
+        return "Enter valid values!"
+
+    def area():
+        return length * width
+
+    def perimeter():
+        return 2 * (length + width)
+
+    return f"Rectangle area: {area()}\n" \
+           f"Rectangle perimeter: {perimeter()}"
