dda4fd45c1a390b56ccb010da0d7059fceee9a3c
Vladislav Naydenov Dimov <99009455+vncmd@users.noreply.github.com>
2023-05-23 14:34:48 +0200

    Python Advanced: Functions Advanced - Lab Problem: n° 5

---
+++ [1.5] Functions Advanced/[0] Functions Advanced - Lab/05_operate.py
@@ -0,0 +1,16 @@
+from functools import reduce
+
+
+def operate(operator, *args):
+
+    if operator == "+":
+        return reduce(lambda a, b: a + b, args)
+
+    elif operator == "-":
+        return reduce(lambda a, b: a - b, args)
+
+    elif operator == "*":
+        return reduce(lambda a, b: a * b, args)
+
+    elif operator == "/":
+        return reduce(lambda a, b: a / b, args)
