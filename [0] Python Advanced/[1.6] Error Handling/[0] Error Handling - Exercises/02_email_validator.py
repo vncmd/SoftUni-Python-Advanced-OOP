5bb96c6605f1d7d7e7734221cf78c4764bdfd4a6
Vladislav Naydenov Dimov <99009455+vncmd@users.noreply.github.com>
2023-05-28 20:40:15 +0200

    Added different solution.

--- [0] Python Advanced/[1.6] Error Handling/[0] Error Handling - Exercises/02_email_validator.py
+++ [0] Python Advanced/[1.6] Error Handling/[0] Error Handling - Exercises/02_email_validator.py
@@ -37,3 +37,61 @@ while emails_data != "End":
         print("Email is valid")
 
     emails_data = input()
+
+# Different solution:
+
+# from re import findall
+#
+#
+# class NameTooShortError(Exception):
+#     """ Name less than or equal to 4 characters"""
+#     pass
+#
+#
+# class MustContainAtSymbolError(Exception):
+#     """ Email does not have @ """
+#     pass
+#
+#
+# class InvalidDomainError(Exception):
+#     """ The domain is different than .com, .org... """
+#     pass
+#
+#
+# class MoreThanOneAtSymbolError(Exception):
+#     """ Email must contain only one @ """
+#
+#
+# class InvalidNameError(Exception):
+#     """ Name can contain only letters, digits and underscores! """
+#     pass
+#
+#
+# MIN_LENGTH = 4
+# VALID_DOMAINS = (".com", ".bg", ".net", ".org")
+#
+# patten_name = r'\w+'
+# pattern_domain = r'\.[a-z]+'
+#
+# emails_data = input()
+#
+# while emails_data != "End":
+#
+#     if emails_data.count("@") > 1:
+#         raise MoreThanOneAtSymbolError("Email should contain only one @ symbol!")
+#
+#     if len(emails_data.split("@")[0]) < MIN_LENGTH:
+#         raise NameTooShortError("Name must be more than 4 characters")
+#
+#     if "@" not in emails_data:
+#         raise MustContainAtSymbolError("Email must contain @")
+#
+#     if findall(patten_name, emails_data)[0] != emails_data.split("@")[0]:
+#         raise InvalidNameError("Name must be a real name!")
+#
+#     if findall(pattern_domain, emails_data)[-1] not in VALID_DOMAINS:
+#         raise InvalidDomainError("Domain must be one of the following: .com, .bg, .org, .net")
+#
+#     print("Email is valid")
+#
+#     emails_data = input()
