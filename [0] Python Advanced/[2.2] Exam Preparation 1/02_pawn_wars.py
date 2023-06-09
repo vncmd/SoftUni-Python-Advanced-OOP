e96dff1fb635fe02a458eef27df87cf163d7ec96
Vladislav Naydenov Dimov <99009455+vncmd@users.noreply.github.com>
2023-06-09 15:47:53 +0200

    Python Advanced: Exam Preparation 1
    Problem: n:2; Different solution

--- [0] Python Advanced/[2.2] Exam Preparation 1/02_pawn_wars.py
+++ [0] Python Advanced/[2.2] Exam Preparation 1/02_pawn_wars.py
@@ -100,36 +100,36 @@ for _ in range(8):
 # Different solution:
 
 
-def save_pos(search_for, index_to_save, r):
-    if search_for in board[r]:
-        positions[index_to_save].append(r)
-        positions[index_to_save].append(board[r].index(search_for))
-
-
-SIZE = 8
-
-board = []
-positions = [[], []]
-
-for row in range(SIZE):
-    board.append(input().split())
-
-    save_pos("w", 0, row)
-    save_pos("b", 1, row)
-
-if abs(positions[0][1] - positions[1][1]) != 1 or positions[1][0] > positions[0][0]:
-
-    if SIZE - positions[0][0] - 1 <= positions[1][0]:
-        print(f"Game over! Black pawn is promoted to a queen at {chr(97 + positions[1][1])}1.")
-
-    else:
-        print(f"Game over! White pawn is promoted to a queen at {chr(97 + positions[0][1])}8.")
-
-else:
-    place = (positions[0][0] + positions[1][0]) // 2
-
-    if positions[0][0] % 2 == positions[1][0] % 2:
-        print(f"Game over! Black win, capture on {chr(97 + positions[0][1])}{SIZE - place}.")
-
-    else:
-        print(f"Game over! White win, capture on {chr(97 + positions[1][1])}{SIZE - place}.")
+# def save_pos(search_for, index_to_save, r):
+#     if search_for in board[r]:
+#         positions[index_to_save].append(r)
+#         positions[index_to_save].append(board[r].index(search_for))
+#
+#
+# SIZE = 8
+#
+# board = []
+# positions = [[], []]
+#
+# for row in range(SIZE):
+#     board.append(input().split())
+#
+#     save_pos("w", 0, row)
+#     save_pos("b", 1, row)
+#
+# if abs(positions[0][1] - positions[1][1]) != 1 or positions[1][0] > positions[0][0]:
+#
+#     if SIZE - positions[0][0] - 1 <= positions[1][0]:
+#         print(f"Game over! Black pawn is promoted to a queen at {chr(97 + positions[1][1])}1.")
+#
+#     else:
+#         print(f"Game over! White pawn is promoted to a queen at {chr(97 + positions[0][1])}8.")
+#
+# else:
+#     place = (positions[0][0] + positions[1][0]) // 2
+#
+#     if positions[0][0] % 2 == positions[1][0] % 2:
+#         print(f"Game over! Black win, capture on {chr(97 + positions[0][1])}{SIZE - place}.")
+#
+#     else:
+#         print(f"Game over! White win, capture on {chr(97 + positions[1][1])}{SIZE - place}.")
