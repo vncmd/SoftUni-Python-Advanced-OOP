79c5db9922e0d026098e96c3c5f1b1bd2660ec83
Vladislav Naydenov Dimov <99009455+vncmd@users.noreply.github.com>
2023-06-19 20:21:06 +0200

    Python Advanced: File Handling - Lab
    Problem: n: 2

---
+++ [0] Python Advanced/[1.7] File Handling/[0] File Handling - Lab/02_file_reader.py
@@ -0,0 +1,11 @@
+import os
+
+absolute_path = os.path.dirname(os.path.abspath(__file__))
+
+file_path = os.path.join(absolute_path, 'files', 'text.txt')
+
+try:
+    file_obj = open(file_path)
+    print('File found')
+except FileNotFoundError:
+    print('File not found')
