b166e1f1400a3a53c1dcc171487008b339fbcf55
Vladislav Naydenov Dimov <99009455+vncmd@users.noreply.github.com>
2023-06-25 20:26:04 +0200

    Python Advanced: Classes and Objects - Exercises
    Problem: n: 1

---
+++ [1] Python OOP/[1.1] Classes and Objects/[1] Classes and Objects - Exercises/01_vet.py
@@ -0,0 +1,50 @@
+class Vet:
+    animals = []
+    space = 5
+
+    def __init__(self, name):
+        self.name = name
+        self.animals = []
+
+    def register_animal(self, animal_name):
+        space_left_in_clinic = Vet.space - len(Vet.animals)
+
+        if space_left_in_clinic > 0:
+            Vet.animals.append(animal_name)
+            self.animals.append(animal_name)
+
+            return f"{animal_name} registered in the clinic"
+
+        return "Not enough space"
+
+    def unregister_animal(self, animal_name):
+        if animal_name in Vet.animals:
+            Vet.animals.remove(animal_name)
+            self.animals.remove(animal_name)
+
+            return f"{animal_name} unregistered successfully"
+
+        return f"{animal_name} not in the clinic"
+
+    def info(self):
+        space_left_in_clinic = Vet.space - len(Vet.animals)
+        return f"{self.name} has {len(self.animals)} animals. {space_left_in_clinic} space left in clinic"
+
+
+# Tests:
+
+peter = Vet("Peter")
+george = Vet("George")
+
+print(peter.register_animal("Tom"))
+print(george.register_animal("Cory"))
+print(peter.register_animal("Fishy"))
+print(peter.register_animal("Bobby"))
+print(george.register_animal("Kay"))
+print(george.unregister_animal("Cory"))
+print(peter.register_animal("Silky"))
+print(peter.unregister_animal("Molly"))
+print(peter.unregister_animal("Tom"))
+
+print(peter.info())
+print(george.info())
