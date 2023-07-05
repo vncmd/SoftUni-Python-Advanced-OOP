1880bfac15fe6f8bb6a4e0212f72dfc9476d126d
Vladislav Naydenov Dimov <99009455+vncmd@users.noreply.github.com>
2023-07-05 18:42:35 +0200

    Python Advanced: Static and Class Methods - Exercise
    Problem: n: 1

---
+++ [1] Python OOP/[1.4] Static and Class Methods/[1] Static and Class Methods - Exercise/01_photo_album.py
@@ -0,0 +1,40 @@
+import math
+
+
+class PhotoAlbum:
+    def __init__(self, pages):
+        self.pages = pages
+        self.photos = self.__initialize_photos()
+        self.current_idx = 0
+
+    def __initialize_photos(self):
+        matrix = [[[] for _ in range(self.pages)]]
+
+        return matrix
+
+    @classmethod
+    def from_photos_count(cls, photos_count):
+        pages = math.ceil(photos_count / 4)
+
+        return cls(pages)
+
+    def add_photo(self, label):
+        if len(self.photos[self.current_idx]) == 4:
+            self.current_idx += 1
+
+        try:
+            self.photos[self.current_idx].append(label)
+            return f"{label} photo added successfully " \
+                   f"on page {self.current_idx + 1} " \
+                   f"slot {len(self.photos[self.current_idx])}"
+
+        except IndexError:
+            return "No more free slots"
+
+    def display(self):
+        res = "-" * 11 + "\n"
+        for page in self.photos:
+            res += " ".join(["[]" for photo_name in page]) + "\n"
+            res += "-" * 11 + "\n"
+
+        return res
