6cfaceb6619649ef1157392a3da635f3daa2022e
Vladislav Naydenov Dimov <99009455+vncmd@users.noreply.github.com>
2023-07-14 21:39:10 +0200

    Python Advanced: SOLID - Lab
    Problem: n: 1; SRP.

---
+++ [1] Python OOP/[1.6] SOLID/[0] SOLID - Lab/01_SRP_books.py
@@ -0,0 +1,49 @@
+from typing import List
+
+
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self.page = 0
+
+    def turn_page(self, page):
+        self.page = page
+
+    def __repr__(self):
+        return f"{self.title} by {self.author}"
+
+
+class Library:
+    def __init__(self):
+        self.books: List[Book] = []
+
+    def add_book(self, book: Book):
+        if book in self.books:
+            return f"The book {book.title} is already in the Library"
+
+        self.books.append(book)
+
+    def remove_book(self, title):
+        try:
+            book = self.find_book(title)
+            self.books.remove(book)
+
+        except ValueError:
+            return f"The book {title} is not in the Library"
+
+        return f"The book {title} by {book.author} was removed from the Library"
+
+    def find_book(self, title):
+        try:
+            book = next(filter(lambda x: x.title == title, self.books))
+        except StopIteration:
+            return "Book not found"
+
+        return book
+
+
+library = Library()
+for number in range(1, 11):
+    book = Book(f"Title {number}", f"Author {number}")
+    library.add_book(book)
