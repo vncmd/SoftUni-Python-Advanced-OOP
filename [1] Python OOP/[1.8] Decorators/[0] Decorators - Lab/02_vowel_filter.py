ec0143e77dad40b1b8aad2d758d2aa01e7c851b6
Vladislav Naydenov Dimov <99009455+vncmd@users.noreply.github.com>
2023-07-20 20:47:31 +0200

    Python Advanced: Decorators - Lab
    Problem: n 2; added different solution.

--- [1] Python OOP/[1.8] Decorators/[0] Decorators - Lab/02_vowel_filter.py
+++ [1] Python OOP/[1.8] Decorators/[0] Decorators - Lab/02_vowel_filter.py
@@ -6,3 +6,21 @@ def vowel_filter(function):
         return [x for x in letters if x in vowels]
 
     return wrapper
+
+# Different solution:
+
+# def vowel_filter(function):
+#     VOWELS = "aeiou"
+#
+#     def wrapper():
+#         return [l for l in function() if l in VOWELS]
+#
+#     return wrapper
+#
+#
+# @vowel_filter
+# def get_letters():
+#     return ["a", "b", "c", "d", "e"]
+#
+#
+# print(get_letters())
