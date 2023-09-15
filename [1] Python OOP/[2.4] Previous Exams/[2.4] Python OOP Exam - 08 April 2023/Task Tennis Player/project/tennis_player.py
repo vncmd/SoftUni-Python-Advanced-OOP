934ec2e9db14d83bdcedbfd3cfcbf1d4f4a8f245
Vladislav Naydenov Dimov <fin@vndimov.com>
2023-09-15 19:30:06 +0200

    Python Advanced: Previous Exams - OOP
    Problem: second part, for Unit Testing; update.
    
    From the Examination of 8th April 2023.

--- [1] Python OOP/[2.4] Previous Exams/[2.4] Python OOP Exam - 08 April 2023/Task Tennis Player/project/tennis_player.py
+++ [1] Python OOP/[2.4] Previous Exams/[2.4] Python OOP Exam - 08 April 2023/Task Tennis Player/project/tennis_player.py
@@ -13,6 +13,7 @@ class TennisPlayer:
     def name(self, value):
         if len(value) <= 2:
             raise ValueError("Name should be more than 2 symbols!")
+
         self.__name = value
 
     @property
@@ -23,17 +24,20 @@ class TennisPlayer:
     def age(self, value):
         if value < 18:
             raise ValueError("Players must be at least 18 years of age!")
+
         self.__age = value
 
     def add_new_win(self, tournament_name: str):
         if tournament_name not in self.wins:
             self.wins.append(tournament_name)
+
         else:
             return f"{tournament_name} has been already added to the list of wins!"
 
     def __lt__(self, other):
         if self.points < other.points:
             return f'{other.name} is a top seeded player and he/she is better than {self.name}'
+
         return f'{self.name} is a better player than {other.name}'
 
     def __str__(self):
